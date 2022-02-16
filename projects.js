403a8064d21eaff57cf8638877c2b03ed695e608
iamfranco <kinfaifrancochan@gmail.com>
2022-02-16 10:06:49 +0000

    add simple login (one server) to project list

--- projects.js
+++ projects.js
@@ -1,74 +1,82 @@
 let projects = [
-    {
-        name: 'FrancoChan.co (this website)',
-        thumbnail_img: 'img/francochanco.svg',
-        thumbnail_gif: 'img/francochanco.gif',
-        link: '',
-        link_code: 'https://github.com/iamfranco/iamfranco.github.io',
-        tags: ['JavaScript', 'HTML', 'CSS'],
-    },
-    {
-        name: 'scrollShow JS Library',
-        thumbnail_img: 'img/scrollShow.svg',
-        thumbnail_gif: 'img/scrollShow.gif',
-        link: 'https://francochan.co/scrollShow/',
-        link_code: 'https://github.com/iamfranco/scrollShow',
-        tags: ['JavaScript', 'HTML', 'CSS'],
-    },
-    {
-        name: 'Raw Talk Official Site',
-        thumbnail_img: 'img/rawTalk.svg',
-        thumbnail_gif: 'img/rawTalk.gif',
-        link: 'https://rawistalk.org/',
-        link_code: 'https://github.com/RawTalk/RawTalk.github.io',
-        tags: ['JavaScript', 'HTML', 'CSS'],
-    },
-    {
-        name: 'Box-like Room Modelling',
-        thumbnail_img: 'img/boxRoom.png',
-        thumbnail_gif: 'img/boxRoom.gif',
-        link: 'https://francochan.co/BoxRoom/',
-        link_code: 'https://github.com/iamfranco/BoxRoom',
-        tags: ['Three.js', 'JavaScript', 'HTML', 'CSS'],
-    },
-    {
-        name: 'Zombie Apocalypse Simulation',
-        thumbnail_img: 'img/zombieInteractive.svg',
-        thumbnail_gif: 'img/zombieInteractive.gif',
-        link: 'https://francochan.co/zombieInteractive/',
-        link_code: 'https://github.com/iamfranco/zombieInteractive',
-        tags: ['D3.js', 'JavaScript', 'HTML', 'CSS'],
-    },
-    {
-        name: 'Game of Life Simulation',
-        thumbnail_img: 'img/gameOfLife.svg',
-        thumbnail_gif: 'img/gameOfLife.gif',
-        link: 'https://francochan.co/game_of_life/',
-        link_code: 'https://github.com/iamfranco/game_of_life',
-        tags: ['JavaScript', 'HTML', 'CSS'],
-    },
-    {
-        name: 'Traffic Wave Simulation',
-        thumbnail_img: 'img/trafficWave.svg',
-        thumbnail_gif: 'img/trafficWave.gif',
-        link: 'https://francochan.co/trafficWave/',
-        link_code: 'https://github.com/iamfranco/trafficWave',
-        tags: ['JavaScript', 'HTML', 'CSS'],
-    },
-    {
-        name: 'Exam Results Visualisation',
-        thumbnail_img: 'img/results_visualise.svg',
-        thumbnail_gif: 'img/results_visualise.gif',
-        link: 'https://francochan.co/results_visualise/',
-        link_code: 'https://github.com/iamfranco/results_visualise',
-        tags: ['D3.js', 'JavaScript', 'HTML', 'CSS'],
-    },
-    {
-        name: 'Maths Questions Generator',
-        thumbnail_img: 'img/mathPDF.svg',
-        thumbnail_gif: 'img/mathPDF.gif',
-        link: 'https://francochan.co/mathPDF/',
-        link_code: 'https://github.com/iamfranco/mathPDF',
-        tags: ['JsPDF', 'JavaScript', 'HTML', 'CSS'],
-    },
+  {
+    name: "Simple Login (One server)",
+    thumbnail_img: "img/simpleLoginOne.png",
+    thumbnail_gif: "img/simpleLoginOne.gif",
+    link: "https://francochanco-simple-login-one.herokuapp.com",
+    link_code: "https://github.com/iamfranco/simple-login-one-server",
+    tags: ["MongoDB", "Express", "React", "Node"]
+  },
+  {
+    name: "FrancoChan.co (this website)",
+    thumbnail_img: "img/francochanco.svg",
+    thumbnail_gif: "img/francochanco.gif",
+    link: "",
+    link_code: "https://github.com/iamfranco/iamfranco.github.io",
+    tags: ["JavaScript", "HTML", "CSS"]
+  },
+  {
+    name: "scrollShow JS Library",
+    thumbnail_img: "img/scrollShow.svg",
+    thumbnail_gif: "img/scrollShow.gif",
+    link: "https://francochan.co/scrollShow/",
+    link_code: "https://github.com/iamfranco/scrollShow",
+    tags: ["JavaScript", "HTML", "CSS"]
+  },
+  {
+    name: "Raw Talk Official Site",
+    thumbnail_img: "img/rawTalk.svg",
+    thumbnail_gif: "img/rawTalk.gif",
+    link: "https://rawistalk.org/",
+    link_code: "https://github.com/RawTalk/RawTalk.github.io",
+    tags: ["JavaScript", "HTML", "CSS"]
+  },
+  {
+    name: "Box-like Room Modelling",
+    thumbnail_img: "img/boxRoom.png",
+    thumbnail_gif: "img/boxRoom.gif",
+    link: "https://francochan.co/BoxRoom/",
+    link_code: "https://github.com/iamfranco/BoxRoom",
+    tags: ["Three.js", "JavaScript", "HTML", "CSS"]
+  },
+  {
+    name: "Zombie Apocalypse Simulation",
+    thumbnail_img: "img/zombieInteractive.svg",
+    thumbnail_gif: "img/zombieInteractive.gif",
+    link: "https://francochan.co/zombieInteractive/",
+    link_code: "https://github.com/iamfranco/zombieInteractive",
+    tags: ["D3.js", "JavaScript", "HTML", "CSS"]
+  },
+  {
+    name: "Game of Life Simulation",
+    thumbnail_img: "img/gameOfLife.svg",
+    thumbnail_gif: "img/gameOfLife.gif",
+    link: "https://francochan.co/game_of_life/",
+    link_code: "https://github.com/iamfranco/game_of_life",
+    tags: ["JavaScript", "HTML", "CSS"]
+  },
+  {
+    name: "Traffic Wave Simulation",
+    thumbnail_img: "img/trafficWave.svg",
+    thumbnail_gif: "img/trafficWave.gif",
+    link: "https://francochan.co/trafficWave/",
+    link_code: "https://github.com/iamfranco/trafficWave",
+    tags: ["JavaScript", "HTML", "CSS"]
+  },
+  {
+    name: "Exam Results Visualisation",
+    thumbnail_img: "img/results_visualise.svg",
+    thumbnail_gif: "img/results_visualise.gif",
+    link: "https://francochan.co/results_visualise/",
+    link_code: "https://github.com/iamfranco/results_visualise",
+    tags: ["D3.js", "JavaScript", "HTML", "CSS"]
+  },
+  {
+    name: "Maths Questions Generator",
+    thumbnail_img: "img/mathPDF.svg",
+    thumbnail_gif: "img/mathPDF.gif",
+    link: "https://francochan.co/mathPDF/",
+    link_code: "https://github.com/iamfranco/mathPDF",
+    tags: ["JsPDF", "JavaScript", "HTML", "CSS"]
+  }
 ]
